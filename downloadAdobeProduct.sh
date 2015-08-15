1308b57a4690346f7532e88aeb3c1e55fed606f8
larryhou <larryhou@foxmail.com>
2015-08-15 16:16:55 +0800

    Create downloadAdobeProduct.sh

---
+++ downloadAdobeProduct.sh
@@ -0,0 +1,140 @@
+#!/bin/bash
+
+# Adobe Photoshop CC 2015
+# http://trials3.adobe.com/AdobeProducts/PHSP/16/win32/Photoshop_16_LS20_win32.7z
+# http://trials3.adobe.com/AdobeProducts/PHSP/16/win64/Photoshop_16_LS20_win64.7z
+# http://trials3.adobe.com/AdobeProducts/PHSP/16/osx10/Photoshop_16_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/PHSP/16/win32/Photoshop_16_LS20_win32.7z
+# http://ccmdl.adobe.com/AdobeProducts/PHSP/16/win64/Photoshop_16_LS20_win64.7z
+# http://ccmdl.adobe.com/AdobeProducts/PHSP/16/osx10/Photoshop_16_LS20.dmg
+#
+# Adobe Illustrator CC 2015
+# http://trials3.adobe.com/AdobeProducts/ILST/19/win32/Illustrator_19_LS20_win32.7z
+# http://trials3.adobe.com/AdobeProducts/ILST/19/win64/Illustrator_19_LS20_win64.7z
+# http://trials3.adobe.com/AdobeProducts/ILST/19/osx10-64/Illustrator_19_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/ILST/19/win32/Illustrator_19_LS20_win32.7z
+# http://ccmdl.adobe.com/AdobeProducts/ILST/19/win64/Illustrator_19_LS20_win64.7z
+# http://ccmdl.adobe.com/AdobeProducts/ILST/19/osx10-64/Illustrator_19_LS20.dmg
+#
+# Adobe InDesign CC 2015
+# http://trials3.adobe.com/AdobeProducts/IDSN/11/win32/InDesign_11_LS20_Win32.7z
+# http://trials3.adobe.com/AdobeProducts/IDSN/11/win64/InDesign_11_LS20_Win64.7z
+# http://trials3.adobe.com/AdobeProducts/IDSN/11/osx10/InDesign_11_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/IDSN/11/win32/InDesign_11_LS20_Win32.7z
+# http://ccmdl.adobe.com/AdobeProducts/IDSN/11/win64/InDesign_11_LS20_Win64.7z
+# http://ccmdl.adobe.com/AdobeProducts/IDSN/11/osx10/InDesign_11_LS20.dmg
+#
+# Adobe Photoshop Lightroom 6 (CC 2015)
+# http://trials3.adobe.com/AdobeProducts/LTRM/6/win64/Lightroom_6_LS11.exe
+# http://trials3.adobe.com/AdobeProducts/LTRM/6/osx10/Lightroom_6_LS11.dmg
+# http://ccmdl.adobe.com/AdobeProducts/LTRM/6/win64/Lightroom_6_LS11.exe
+# http://ccmdl.adobe.com/AdobeProducts/LTRM/6/osx10/Lightroom_6_LS11.dmg
+#
+# Adobe InCopy CC 2015
+# http://trials3.adobe.com/AdobeProducts/AICY/11/win32/InCopy_11_LS20_Win32.7z
+# http://trials3.adobe.com/AdobeProducts/AICY/11/win64/InCopy_11_LS20_Win64.7z
+# http://trials3.adobe.com/AdobeProducts/AICY/11/osx10/InCopy_11_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/AICY/11/win32/InCopy_11_LS20_Win32.7z
+# http://ccmdl.adobe.com/AdobeProducts/AICY/11/win64/InCopy_11_LS20_Win64.7z
+# http://ccmdl.adobe.com/AdobeProducts/AICY/11/osx10/InCopy_11_LS20.dmg
+#
+# Adobe Edge Animate CC 2015
+# http://trials3.adobe.com/AdobeProducts/EDGE/6/win64/Edge_Animate_6_0_0_LS17.exe
+# http://trials3.adobe.com/AdobeProducts/EDGE/6/osx10-64/Edge_Animate_6_0_0_LS17.dmg
+# http://ccmdl.adobe.com/AdobeProducts/EDGE/6/win64/Edge_Animate_6_0_0_LS17.exe
+# http://ccmdl.adobe.com/AdobeProducts/EDGE/6/osx10-64/Edge_Animate_6_0_0_LS17.dmg
+#
+# Adobe SpeedGrade CC 2015
+# http://trials3.adobe.com/AdobeProducts/SPGD/9/win64/SpeedGrade_9_LS20.exe
+# http://trials3.adobe.com/AdobeProducts/SPGD/9/osx10-64/SpeedGrade_9_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/SPGD/9/win64/SpeedGrade_9_LS20.exe
+# http://ccmdl.adobe.com/AdobeProducts/SPGD/9/osx10-64/SpeedGrade_9_LS20.dmg
+#
+# Adobe Prelude CC 2015
+# http://trials3.adobe.com/AdobeProducts/PRLD/4/win64/Prelude_4_LS20.exe
+# http://trials3.adobe.com/AdobeProducts/PRLD/4/osx10-64/Prelude_4_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/PRLD/4/win64/Prelude_4_LS20.exe
+# http://ccmdl.adobe.com/AdobeProducts/PRLD/4/osx10-64/Prelude_4_LS20.dmg
+#
+# Adobe Audition CC 2015
+# http://trials3.adobe.com/AdobeProducts/AUDT/8/win64/Audition_8_LS20.exe
+# http://trials3.adobe.com/AdobeProducts/AUDT/8/osx10-64/Audition_8_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/AUDT/8/win64/Audition_8_LS20.exe
+# http://ccmdl.adobe.com/AdobeProducts/AUDT/8/osx10-64/Audition_8_LS20.dmg
+#
+# Adobe Flash Pro CC 2015
+# http://trials3.adobe.com/AdobeProducts/FLPR/15/win64/Flash_Professional_15_LS20.exe
+# http://trials3.adobe.com/AdobeProducts/FLPR/15/osx10-64/Flash_Professional_15_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/FLPR/15/win64/Flash_Professional_15_LS20.exe
+# http://ccmdl.adobe.com/AdobeProducts/FLPR/15/osx10-64/Flash_Professional_15_LS20.dmg
+#
+# Adobe After Effects CC 2015
+# http://trials3.adobe.com/AdobeProducts/AEFT/14/win64/AfterEffects_14_LS20.7z
+# http://trials3.adobe.com/AdobeProducts/AEFT/14/osx10-64/AfterEffects_14_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/AEFT/14/win64/AfterEffects_14_LS20.7z
+# http://ccmdl.adobe.com/AdobeProducts/AEFT/14/osx10-64/AfterEffects_14_LS20.dmg
+#
+# Adobe Dreamweaver CC 2015
+# http://trials3.adobe.com/AdobeProducts/DRWV/16/win32/Dreamweaver_16_LS20.exe
+# http://trials3.adobe.com/AdobeProducts/DRWV/16/win64/Dreamweaver_16_LS20.exe
+# http://trials3.adobe.com/AdobeProducts/DRWV/16/osx10-64/Dreamweaver_16_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/DRWV/16/win32/Dreamweaver_16_LS20.exe
+# http://ccmdl.adobe.com/AdobeProducts/DRWV/16/win64/Dreamweaver_16_LS20.exe
+# http://ccmdl.adobe.com/AdobeProducts/DRWV/16/osx10-64/Dreamweaver_16_LS20.dmg
+#
+# Adobe Premiere Pro CC 2015
+# http://trials3.adobe.com/AdobeProducts/PPRO/9/win64/PremierePro_9_LS20.7z
+# http://trials3.adobe.com/AdobeProducts/PPRO/9/osx10-64/PremierePro_9_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/PPRO/9/win64/PremierePro_9_LS20.7z
+# http://ccmdl.adobe.com/AdobeProducts/PPRO/9/osx10-64/PremierePro_9_LS20.dmg
+#
+# Adobe Media Encoder CC 2015
+# http://trials3.adobe.com/AdobeProducts/AME/9/win64/AdobeMediaEncoder_9_LS20.exe
+# http://trials3.adobe.com/AdobeProducts/AME/9/osx10-64/AdobeMediaEncoder_9_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/AME/9/win64/AdobeMediaEncoder_9_LS20.exe
+# http://ccmdl.adobe.com/AdobeProducts/AME/9/osx10-64/AdobeMediaEncoder_9_LS20.dmg
+#
+# Adobe Muse CC 2015
+# http://trials3.adobe.com/AdobeProducts/MUSE/2015/win64/Muse_2015_0_CC_LS24.7z
+# http://trials3.adobe.com/AdobeProducts/MUSE/2015/osx10-64/Muse_2015_0_CC_LS24.dmg
+# http://ccmdl.adobe.com/AdobeProducts/MUSE/2015/win64/Muse_2015_0_CC_LS24.7z
+# http://ccmdl.adobe.com/AdobeProducts/MUSE/2015/osx10-64/Muse_2015_0_CC_LS24.dmg
+#
+# Adobe Captivate 9
+# http://trials3.adobe.com/AdobeProducts/CPTV/9/win32/Captivate_9_LS21.7z
+# http://trials3.adobe.com/AdobeProducts/CPTV/9/win64/Captivate_9_x64_LS21.7z
+# http://trials3.adobe.com/AdobeProducts/CPTV/9/osx10/Captivate_9_LS21.dmg
+# http://ccmdl.adobe.com/AdobeProducts/CPTV/9/win32/Captivate_9_LS21.7z
+# http://ccmdl.adobe.com/AdobeProducts/CPTV/9/win64/Captivate_9_x64_LS21.7z
+# http://ccmdl.adobe.com/AdobeProducts/CPTV/9/osx10/Captivate_9_LS21.dmg
+#
+# Adobe Photoshop Elements 14
+# http://trials3.adobe.com/AdobeProducts/PSE/14/win32/PhotoshopElements_14_LS25_win32.7z
+# http://trials3.adobe.com/AdobeProducts/PSE/14/win64/PhotoshopElements_14_LS25_win64.7z
+# http://trials3.adobe.com/AdobeProducts/PSE/14/osx10/PhotoshopElements_14_WWEFDJ.dmg
+# http://ccmdl.adobe.com/AdobeProducts/PSE/14/win32/PhotoshopElements_14_LS25_win32.7z
+# http://ccmdl.adobe.com/AdobeProducts/PSE/14/win64/PhotoshopElements_14_LS25_win64.7z
+# http://ccmdl.adobe.com/AdobeProducts/PSE/14/osx10/PhotoshopElements_14_WWEFDJ.dmg
+#
+# Adobe Premiere Elements 14
+# http://trials3.adobe.com/AdobeProducts/PRE/14/win32/PremiereElements_14_LS26_win32.7z
+# http://trials3.adobe.com/AdobeProducts/PRE/14/win64/PremiereElements_14_LS26_win64.7z
+# http://trials3.adobe.com/AdobeProducts/PRE/14/osx10/PremiereElements_14_WWEFDJ.dmg
+# http://ccmdl.adobe.com/AdobeProducts/PRE/14/win32/PremiereElements_14_LS26_win32.7z
+# http://ccmdl.adobe.com/AdobeProducts/PRE/14/win64/PremiereElements_14_LS26_win64.7z
+# http://ccmdl.adobe.com/AdobeProducts/PRE/14/osx10/PremiereElements_14_WWEFDJ.dmg
+
+url='http://ccmdl.adobe.com/AdobeProducts/PHSP/16/osx10/Photoshop_16_LS20.dmg'
+
+function download()
+{
+	name=$(echo ${1} | awk -F/ '{print $NF}')
+	wget --server-response \
+		 --header='User-Agent: Adobe Application Manager 2.0' \
+		 --output-document ${2}/${name} ${1}
+	
+	# axel -H 'User-Agent: Adobe Application Manager 2.0' \
+	# 	 ${1} -o ${2}/${name}
+}
+
+download ${url} ~/Downloads
