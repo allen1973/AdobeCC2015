cea5cfae67878fa8823ecabca7177ec363168b40
larryhou <larryhou@foxmail.com>
2015-08-17 13:50:04 +0800

    Update downloadAdobeCC2015.sh

--- downloadAdobeCC2015.sh
+++ downloadAdobeCC2015.sh
@@ -24,11 +24,17 @@
 # http://ccmdl.adobe.com/AdobeProducts/IDSN/11/win64/InDesign_11_LS20_Win64.7z
 # http://ccmdl.adobe.com/AdobeProducts/IDSN/11/osx10/InDesign_11_LS20.dmg
 #
-# Adobe Photoshop Lightroom 6 (CC 2015)
-# http://trials3.adobe.com/AdobeProducts/LTRM/6/win64/Lightroom_6_LS11.exe
-# http://trials3.adobe.com/AdobeProducts/LTRM/6/osx10/Lightroom_6_LS11.dmg
-# http://ccmdl.adobe.com/AdobeProducts/LTRM/6/win64/Lightroom_6_LS11.exe
-# http://ccmdl.adobe.com/AdobeProducts/LTRM/6/osx10/Lightroom_6_LS11.dmg
+# Adobe Premiere Pro CC 2015
+# http://trials3.adobe.com/AdobeProducts/PPRO/9/win64/PremierePro_9_LS20.7z
+# http://trials3.adobe.com/AdobeProducts/PPRO/9/osx10-64/PremierePro_9_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/PPRO/9/win64/PremierePro_9_LS20.7z
+# http://ccmdl.adobe.com/AdobeProducts/PPRO/9/osx10-64/PremierePro_9_LS20.dmg
+#
+# Adobe After Effects CC 2015
+# http://trials3.adobe.com/AdobeProducts/AEFT/13_5/win64/AfterEffects_13_5_LS20.7z
+# http://trials3.adobe.com/AdobeProducts/AEFT/13_5/osx10-64/AfterEffects_13_5_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/AEFT/13_5/win64/AfterEffects_13_5_LS20.7z
+# http://ccmdl.adobe.com/AdobeProducts/AEFT/13_5/osx10-64/AfterEffects_13_5_LS20.dmg
 #
 # Adobe InCopy CC 2015
 # http://trials3.adobe.com/AdobeProducts/AICY/11/win32/InCopy_11_LS20_Win32.7z
@@ -68,12 +74,6 @@
 # http://ccmdl.adobe.com/AdobeProducts/FLPR/15/win64/Flash_Professional_15_LS20.exe
 # http://ccmdl.adobe.com/AdobeProducts/FLPR/15/osx10-64/Flash_Professional_15_LS20.dmg
 #
-# Adobe After Effects CC 2015
-# http://trials3.adobe.com/AdobeProducts/AEFT/14/win64/AfterEffects_14_LS20.7z
-# http://trials3.adobe.com/AdobeProducts/AEFT/14/osx10-64/AfterEffects_14_LS20.dmg
-# http://ccmdl.adobe.com/AdobeProducts/AEFT/14/win64/AfterEffects_14_LS20.7z
-# http://ccmdl.adobe.com/AdobeProducts/AEFT/14/osx10-64/AfterEffects_14_LS20.dmg
-#
 # Adobe Dreamweaver CC 2015
 # http://trials3.adobe.com/AdobeProducts/DRWV/16/win32/Dreamweaver_16_LS20.exe
 # http://trials3.adobe.com/AdobeProducts/DRWV/16/win64/Dreamweaver_16_LS20.exe
@@ -81,12 +81,6 @@
 # http://ccmdl.adobe.com/AdobeProducts/DRWV/16/win32/Dreamweaver_16_LS20.exe
 # http://ccmdl.adobe.com/AdobeProducts/DRWV/16/win64/Dreamweaver_16_LS20.exe
 # http://ccmdl.adobe.com/AdobeProducts/DRWV/16/osx10-64/Dreamweaver_16_LS20.dmg
-#
-# Adobe Premiere Pro CC 2015
-# http://trials3.adobe.com/AdobeProducts/PPRO/9/win64/PremierePro_9_LS20.7z
-# http://trials3.adobe.com/AdobeProducts/PPRO/9/osx10-64/PremierePro_9_LS20.dmg
-# http://ccmdl.adobe.com/AdobeProducts/PPRO/9/win64/PremierePro_9_LS20.7z
-# http://ccmdl.adobe.com/AdobeProducts/PPRO/9/osx10-64/PremierePro_9_LS20.dmg
 #
 # Adobe Media Encoder CC 2015
 # http://trials3.adobe.com/AdobeProducts/AME/9/win64/AdobeMediaEncoder_9_LS20.exe
@@ -123,6 +117,77 @@
 # http://ccmdl.adobe.com/AdobeProducts/PRE/14/win32/PremiereElements_14_LS26_win32.7z
 # http://ccmdl.adobe.com/AdobeProducts/PRE/14/win64/PremiereElements_14_LS26_win64.7z
 # http://ccmdl.adobe.com/AdobeProducts/PRE/14/osx10/PremiereElements_14_WWEFDJ.dmg
+#
+# Adobe Photoshop Lightroom 6
+# http://trials3.adobe.com/AdobeProducts/LTRM/6/win64/Lightroom_6_LS11.exe
+# http://trials3.adobe.com/AdobeProducts/LTRM/6/osx10/Lightroom_6_LS11.dmg
+# http://ccmdl.adobe.com/AdobeProducts/LTRM/6/win64/Lightroom_6_LS11.exe
+# http://ccmdl.adobe.com/AdobeProducts/LTRM/6/osx10/Lightroom_6_LS11.dmg
+#
+# Acrobat Pro DC v12
+#
+# http://trials3.adobe.com/AdobeProducts/APRO/Solstice_Acrobat2015/win32/Acrobat_2015_Web_WWMUI.exe
+# http://ccmdl.adobe.com/AdobeProducts/APRO/Solstice_Acrobat2015/win32/Acrobat_2015_Web_WWMUI.exe
+#
+# Camera Raw 9.1
+# http://helpx.adobe.com/content/help/en/x-productkb/multi/camera-raw-plug-in-installer/_jcr_content/main-pars/download/file.res/CameraRaw_9_1.zip
+# http://helpx.adobe.com/content/help/en/x-productkb/multi/camera-raw-plug-in-installer/_jcr_content/main-pars/download_0/file.res/CameraRaw_9_1.pkg.zip
+#
+# Bridge CC 2013 v6.1
+# http://trials3.adobe.com/AdobeProducts/KBRG/6/win32/Bridge_6_LS20_win32.7z
+# http://ccmdl.adobe.com/AdobeProducts/KBRG/6/win32/Bridge_6_LS20_win32.7z
+# http://trials3.adobe.com/AdobeProducts/KBRG/6/osx10/Bridge_6_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/KBRG/6/osx10/Bridge_6_LS20.dmg
+# http://swupdl.adobe.com/updates/oobe/aam20/win/AdobeBridgeCC64Bit-6.0/6.0.1/setup.zip
+# http://swupdl.adobe.com/updates/oobe/aam20/win/AdobeBridgeCC64Bit-6.0/6.1/setup.zip
+#
+# Extension Manager CC 2014 v7.3.2
+# http://trials3.adobe.com/AdobeProducts/KEMN/7/win32/AdobeExtensionManager_CC_LS20.7z
+# http://ccmdl.adobe.com/AdobeProducts/KEMN/7/win32/AdobeExtensionManager_CC_LS20.7z
+# http://trials3.adobe.com/AdobeProducts/KEMN/7/osx10/AdobeExtensionManager_CC_LS20.dmg
+# http://ccmdl.adobe.com/AdobeProducts/KEMN/7/osx10/AdobeExtensionManager_CC_LS20.dmg
+#
+# Adobe Technical Communication Suite v6
+# http://trials3.adobe.com/AdobeProducts/ATST/6/win32/TechCommunicationSuite_6_LREFDJ.7z
+# http://ccmdl.adobe.com/AdobeProducts/ATST/6/win32/TechCommunicationSuite_6_LREFDJ.7z
+#
+# Edge Code Preview 8
+# http://trials3.adobe.com/AdobeProducts/CODE/1/win32/Edge_Code_preview_8_LREF.7z
+# http://ccmdl.adobe.com/AdobeProducts/CODE/1/win32/Edge_Code_preview_8_LREF.7z
+# http://trials3.adobe.com/AdobeProducts/CODE/1/osx10/Edge_Code_preview_8_LREF.dmg
+# http://ccmdl.adobe.com/AdobeProducts/CODE/1/osx10/Edge_Code_preview_8_LREF.dmg
+#
+# Edge Reflow Preview 8.1
+# http://trials3.adobe.com/AdobeProducts/RFLW/2/win32/Edge_Reflow_preview_8.1_LREF.7z
+# http://ccmdl.adobe.com/AdobeProducts/RFLW/2/win32/Edge_Reflow_preview_8.1_LREF.7z
+# http://trials3.adobe.com/AdobeProducts/RFLW/2/win64/Edge_Reflow_preview_8.1_LREF.7z
+# http://ccmdl.adobe.com/AdobeProducts/RFLW/2/win64/Edge_Reflow_preview_8.1_LREF.7z
+# http://trials3.adobe.com/AdobeProducts/RFLW/2/osx10/Edge_Reflow_preview_8.1_LREF.dmg
+# http://ccmdl.adobe.com/AdobeProducts/RFLW/2/osx10/Edge_Reflow_preview_8.1_LREF.dmg
+#
+# FrameMaker v13
+# http://trials3.adobe.com/AdobeProducts/FM/13/win32/Framemaker_13_LREFDJ.7z
+# http://ccmdl.adobe.com/AdobeProducts/FM/13/win32/Framemaker_13_LREFDJ.7z
+#
+# FrameMaker XML Author v13
+# http://trials3.adobe.com/AdobeProducts/FMXA/13/win32/FMXMLAuthor_13_LREFDJ.7z
+# http://ccmdl.adobe.com/AdobeProducts/FMXA/13/win32/FMXMLAuthor_13_LREFDJ.7z
+#
+# FrameMaker Publishing Server v13
+# http://trials3.adobe.com/AdobeProducts/FMSV/13/win32/FramemakerPublishingServer_13_LS1.7z
+# http://ccmdl.adobe.com/AdobeProducts/FMSV/13/win32/FramemakerPublishingServer_13_LS1.7z
+#
+# Presenter v10
+# http://trials3.adobe.com/AdobeProducts/CPTL/10/win32/Presenter_10_LS21.7z
+# http://ccmdl.adobe.com/AdobeProducts/CPTL/10/win32/Presenter_10_LS21.7z
+#
+# RoboHelp v12
+# http://trials3.adobe.com/AdobeProducts/RBHP/12/win32/RoboHelp_12_LREFDJ.7z
+# http://ccmdl.adobe.com/AdobeProducts/RBHP/12/win32/RoboHelp_12_LREFDJ.7z
+#
+# Scout v1
+# http://trials3.adobe.com/AdobeProducts/ASCT/1_1_3/win64/Scout_1_1_3_LREFJC.7z
+# http://ccmdl.adobe.com/AdobeProducts/ASCT/1_1_3/win64/Scout_1_1_3_LREFJC.7z
 
 function download()
 {
@@ -135,7 +200,7 @@ function download()
 	# 	 ${1} -o ${2}/${name}
 }
 
-list='http://ccmdl.adobe.com/AdobeProducts/AEFT/14/osx10-64/AfterEffects_14_LS20.dmg
+list='http://ccmdl.adobe.com/AdobeProducts/AEFT/13_5/osx10-64/AfterEffects_13_5_LS20.dmg
 http://ccmdl.adobe.com/AdobeProducts/AUDT/8/osx10-64/Audition_8_LS20.dmg
 http://ccmdl.adobe.com/AdobeProducts/LTRM/6/osx10/Lightroom_6_LS11.dmg
 http://ccmdl.adobe.com/AdobeProducts/PPRO/9/osx10-64/PremierePro_9_LS20.dmg
